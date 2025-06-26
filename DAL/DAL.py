98352e36b8a7ea9fbac962db3cba0f296280fb30
shmuelblau <a0528075188@gmail.com>
2025-06-26 18:02:00 +0300

    finish docker

--- DAL/DAL.py
+++ DAL/DAL.py
@@ -3,9 +3,9 @@ class DAL:
     def __init__(self , dbname) -> None:
 
         self.conn = mysql.connector.connect(
-        host='localhost',
-        user='intel_user',
-        password='intelpass',
+        host='eagle-mysql',
+        user='eagle_user',
+        password='eaglepass',
         database=dbname,
         port=3306,
         auth_plugin='mysql_native_password'
